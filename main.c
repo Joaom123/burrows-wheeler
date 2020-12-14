738c6b630250bb52e7c63fdf9e01af796e931a9c
Elysson Gabriel <elyga8000@gmail.com>
2020-12-13 21:27:08 -0300

    Comentários e ajuste de scanf

--- main.c
+++ main.c
@@ -1,85 +1,106 @@
+/*
+ * @(#)burrows-wheeler.c
+ *
+ * Copyright 2019 by Anureet Kaur,
+ * Geeks for geeks ORG
+ * Free source-code.
+ *
+ */
+/*--------------------------------------------------------------
+--  #1.
+--  Date: Aug, 08, 2019
+--  Author: Anureet Kaur
+--  Motivo: Contribute to students
+-------------------------------------------------------------
+--  #2.
+--  Date: Dec, 11, 2020
+--  Author: Elysson Gabriel e João Marcus
+--  Motivo: Trabalho da disciplina de 
+			Sistemas Embarcados 2020.2, Professor: Elias Teodoro
+-------------------------------------------------------------
+**/
+/**
+A transformada de Burrows-Wheeler constrói uma matriz n x n onde n é o tamanho do vetor que será comprimido.
+Esta matriz é preenchida na primeira linha com o vetor original; na segunda linha com o vetor rotacionado a esquerda de uma posição;
+na terceira linha com o vetor rotacionado de 2 posições, e assim por diante, até termos na última linha o vetor rotacionado de n-1 posições.
+Desta nova matriz, a última coluna pode ser facilmente comprimida por técnicas de move-to-front.
+ **/
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h> 
-  
-// Structure to store data of a string_com_posicao 
+
+// Struct definida para guardar as informações de índice e texto rotacionado 
 struct string_com_posicao { 
     int posicao; 
     char* string_rotacionada; 
 }; 
   
-// Compares the string_com_posicaos and 
-// sorts the string_com_posicaos alphabetically 
+// Função de comparação para ordenar as string em ordem alfabética
+// Usada pelo qsort na função rotaciona_string()
 int funcao_comparacao(const void* a, const void* b) { 
     struct string_com_posicao* posicao_a = (struct string_com_posicao*)a; 
     struct string_com_posicao* posicao_b = (struct string_com_posicao*)b; 
     return strcmp(posicao_a->string_rotacionada, posicao_b->string_rotacionada); 
 } 
   
-// Takes text to be transformed and its length as 
-// arguments and returns the corresponding string_rotacionada array 
+// Realiza o ciclo de rotações da string informada 
 int* rotaciona_string(char* string_entrada, int tamanho) { 
-    // Array of structures to store string_com_posicaos and 
-    // their posicaoes 
+    // Vetor que armazenará cada linha rotacionada da string e suas posições originais 
     struct string_com_posicao lista_de_strings[tamanho]; 
   
-    // Structure is needed to maintain old posicaoes of 
-    // string_com_posicaos after sorting them 
+    // Preence o vetor string_com_posicao com as linhas rotacionadas 
     for (int i = 0; i < tamanho; i++) { 
         lista_de_strings[i].posicao = i; 
         lista_de_strings[i].string_rotacionada = (string_entrada + i); 
     } 
   
-    // Sorts string_com_posicaos using comparison 
-    // function defined above 
-    qsort(lista_de_strings, tamanho, sizeof(struct string_com_posicao), 
-          funcao_comparacao); 
+    // Coloca os textos de string_com_posicaos em ordem alfabética 
+    qsort(lista_de_strings, tamanho, sizeof(struct string_com_posicao), funcao_comparacao); 
   
-    // Stores the posicaoes of sorted string_com_posicaos 
-    int* string_rotacionada 
-        = (int*)malloc(tamanho * sizeof(int)); 
-    for (int i = 0; i < tamanho; i++) 
+    // Salva as posições ordenadas da lista rotacionada 
+    int string_rotacionada[tamanho]; 
+    for (int i = 0; i < tamanho; i++) { 
         string_rotacionada[i] = lista_de_strings[i].posicao; 
-  
-    // Returns the computed string_rotacionada array 
+  	}
+   
     return string_rotacionada; 
 } 
-  
-// Takes string_rotacionada array and its size 
-// as arguments and returns the 
-// Burrows - Wheeler Transform of given text 
-void burrows_wheeler(char* string_entrada) {
-	printf("Texto a ser tranformado: %s\n", string_entrada);
 
+// Método de Burrows - Wheeler
+char* burrows_wheeler(char* string_entrada) {
     int tamanho = strlen(string_entrada); 
-    // Computes the string_rotacionada array of our text 
+
+    // Realiza as rotações da string original
     int* string_rotacionada = rotaciona_string(string_entrada, tamanho);  
-    // Iterates over the string_rotacionada array to find 
-    // the last char of each cyclic string_com_posicao 
-    //char* transformada_burrows_wheeler = (char*)malloc(tamanho * sizeof(char));
-    char transformada_burrows_wheeler[40];
+
+    // Vetor a ser preenchido com a última coluna da rotação 
+    char transformada_burrows_wheeler[tamanho];
+
+    // Preenche o vetor anterior com o último caractere de cada rotação
     int i; 
-    for (i = 0; i < tamanho; i++) { 
-        // Computes the last char which is given by 
-        // string_entrada[(string_rotacionada[i] + n - 1) % n] 
+    for (i = 0; i < tamanho; i++) {   
         int j = string_rotacionada[i] - 1; 
-        if (j < 0) 
+        if (j < 0) {
             j = j + tamanho; 
-  
+        }
         transformada_burrows_wheeler[i] = string_entrada[j]; 
     } 
   
     transformada_burrows_wheeler[i] = '\0'; 
-    printf("Método de Burrows-Wheeler: %s\n", transformada_burrows_wheeler);
-    // Returns the computed Burrows - Wheeler Transform  
+    // Retorna o resultado da transformada de Burrows-Wheeler 
+    return transformada_burrows_wheeler; 
 } 
   
-// Driver program to test functions above 
 int main() {
+	// Realiza a leitura da entrada
     char string_entrada[45];
     scanf("%s", string_entrada);
+    // A tranformada de burrows-wheeler também funciona sem o $, porém concatemos a string original
+    // devido ao nosso meio de validação, que realiza a transformada inversa utilizando o ‘$’ como EOF.
     strcat(string_entrada, "$");
-    // Adds to the output array the last char 
-    // of each string_com_posicao 
-    burrows_wheeler(string_entrada);
+    printf("Texto a ser tranformado: %s\n", string_entrada);
+    // Chama o BWT, passando a entrada como parâmetro
+    char* string_saida = burrows_wheeler(string_entrada);
+    // Mostra o resultado da transformada
+    printf("Método de Burrows-Wheeler: %s\n", string_saida);
 } 
