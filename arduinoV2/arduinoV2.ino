e1dbfa904d79f2b579836e5e83f356b642f8ca69
Elysson Gabriel <elyga8000@gmail.com>
2021-02-04 11:26:25 -0300

    Mais ajustes

--- arduinoV2/arduinoV2.ino
+++ arduinoV2/arduinoV2.ino
@@ -1,17 +1,17 @@
 #define TAMANHO_MAXIMO_DA_ENTRADA 36
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
-//String recebi;
+// vetor que armazena os dados vindos da Serial
 char string_entrada[TAMANHO_MAXIMO_DA_ENTRADA];
 
 //vetor temporário que armazena o deslocamentos
-static char tmp[TAMANHO_MAXIMO_DA_ENTRADA] = "";
+char tmp[TAMANHO_MAXIMO_DA_ENTRADA] = "";
+
 // string de copia para não afetar a original
-static char copia[TAMANHO_MAXIMO_DA_ENTRADA] = "";
+char copia[TAMANHO_MAXIMO_DA_ENTRADA] = "";
 
 // Struct definida para guardar as informações de índice e texto rotacionado
 struct string_com_posicao {
@@ -20,36 +20,37 @@ struct string_com_posicao {
 };
 
 // Vetor que armazenará cada linha rotacionada da string e suas posições originais
-// Espaço alocado de 3525 chars, pois é um vetor de string, ou seja,
-// Cada string contem 35 chars, e o vetor possui 35 strings.
+// Espaço alocado de 1296 chars, pois é um vetor de string, ou seja,
+// Cada string contem 36 chars, e o vetor possui 36 strings.
 struct string_com_posicao lista_de_strings[TAMANHO_MAXIMO_DA_ENTRADA];
 
 // Salva as posições ordenadas da lista rotacionada
-static char string_rotacionada[TAMANHO_MAXIMO_DA_ENTRADA];
+char string_rotacionada[TAMANHO_MAXIMO_DA_ENTRADA];
 
 int len, new_idx;
 int i = 0, j = 0, k = 0;
+int qtd_posicoes = 0;
 
-char* desloca_posicao(char* string, int quantidade)
+void desloca_posicao()
 {
-    strcpy(copia, string);
+    strcpy(copia, string_entrada);
     strcpy(tmp, copia);
     len = strlen(copia);
-    if (quantidade < 0)
-        quantidade = len + (quantidade % len);
+    if (qtd_posicoes < 0)
+        qtd_posicoes = len + (qtd_posicoes % len);
     for (i = 0; copia[i] != 0; i++) {
-        new_idx = (i + quantidade) % len;
+        new_idx = (i + qtd_posicoes) % len;
         tmp[new_idx] = copia[i];
     }
     memcpy(copia, tmp, len);
-    return tmp;
+    //return tmp;
 }
-
-struct string_com_posicao* posicao_a;
-struct string_com_posicao* posicao_b;
 
 // Função de comparação para ordenar as string em ordem alfabética
 // Usada pelo qsort na função rotaciona_string()
+struct string_com_posicao* posicao_a;
+struct string_com_posicao* posicao_b;
+
 int funcao_comparacao(const void* a, const void* b) {
     posicao_a = (struct string_com_posicao*)a;
     posicao_b = (struct string_com_posicao*)b;
@@ -57,33 +58,25 @@ int funcao_comparacao(const void* a, const void* b) {
 }
 
 // Realiza o ciclo de rotações da string informada
-char* transforma_string(char* string_entrada, int tamanho) {
+int tamanho = 0;
+void burrows_wheeler() {
     // Preence o vetor string_com_posicao com as linhas rotacionadas
+    tamanho = strlen(string_entrada);
     for (j = 0; j < tamanho; j++) {
         lista_de_strings[j].posicao = j;
-        strcpy(lista_de_strings[j].string_rotacionada, desloca_posicao(string_entrada, -j));
+        qtd_posicoes = -j;
+        desloca_posicao();
+        strcpy(lista_de_strings[j].string_rotacionada, tmp);
     }
     
     // Coloca os textos de string_com_posicaos em ordem alfabética
     qsort(lista_de_strings, tamanho, sizeof(struct string_com_posicao), funcao_comparacao);
     
-    //Serial.println(lista_de_strings[3].string_rotacionada);
     memset(string_rotacionada, 0, sizeof(string_rotacionada));
-    for (k = 0; k < strlen(string_entrada); k++) {
+    for (k = 0; k < tamanho; k++) {
         string_rotacionada[k] = lista_de_strings[k].string_rotacionada[tamanho-1];
     }
-    return string_rotacionada;
 }
-/*
-// Método de Burrows - Wheeler
-char* burrows_wheeler(char* string_entrada) {
-    int tamanho = strlen(string_entrada);
-
-    // Realiza as rotações da string original
-    char* transformada_burrows_wheeler = transforma_string(string_entrada, tamanho);
-    
-    return transformada_burrows_wheeler;
-}*/
 
 void loop() {
   if(Serial.available() > 0){
@@ -98,7 +91,8 @@ void loop() {
       Serial.print("Texto a ser tranformado: ");
       Serial.println(string_entrada);
       Serial.print("Método de Burrows-Wheeler: ");
-      Serial.println(transforma_string(string_entrada, strlen(string_entrada)));
+      burrows_wheeler();
+      Serial.println(string_rotacionada);
     }
   }
 }
