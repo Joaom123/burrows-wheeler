0f282c84433518e10ce8e0ee857d183d0c1dedb9
Elysson Gabriel <elyga8000@gmail.com>
2021-02-08 20:46:08 -0300

    documentação

--- arduinoV2/arduinoV2.ino
+++ arduinoV2/arduinoV2.ino
@@ -1,5 +1,47 @@
-#define TAMANHO_MAXIMO_DA_ENTRADA 37
+/*
+ * @(#)burrows-wheeler.ino
+ *
+ * Copyright 2019 by Anureet Kaur,
+ * Geeks for geeks ORG
+ * Free source-code.
+ *
+ */
+/*--------------------------------------------------------------
+--  #1.
+--  Date: Aug, 08, 2019
+--  Author: Anureet Kaur
+--  Motivo: Contribute to students
+--  Código legado: https://www.geeksforgeeks.org/burrows-wheeler-data-transform-algorithm/
+-------------------------------------------------------------
+--  #2.
+--  Date: Dec, 11, 2020
+--  Author: Elysson Gabriel e João Marcus
+--  Motivo: Trabalho da disciplina de
+      Sistemas Embarcados 2020.2, Professor: Elias Teodoro
+-------------------------------------------------------------
+--  #3.
+--  Date: Jan, 7, 2021
+--  Author: Elysson Gabriel e João Marcus
+--  Motivo: Trabalho da disciplina de
+      Sistemas Embarcados 2020.2, Professor: Elias Teodoro
+--  Adaptação realizada para migrar o código .c para o microcontrolador Arduino UNO
+-------------------------------------------------------------
+**/
+/**
+A transformada de Burrows-Wheeler constrói uma matriz n x n onde n é o tamanho do vetor que será comprimido.
+Esta matriz é preenchida na primeira linha com o vetor original; na segunda linha com o vetor rotacionado a esquerda de uma posição;
+na terceira linha com o vetor rotacionado de 2 posições, e assim por diante, até termos na última linha o vetor rotacionado de n-1 posições.
+Desta nova matriz, a última coluna pode ser facilmente comprimida por técnicas de move-to-front.
 
+- Modo de uso:
+  A plataforma destinada a executar este código é o microcontrolador Arduino UNO, utilizando como entrada de dados a comunicação Serial e exibindo
+  o resultado do processamento descrito anteriormente também pela mesma. 
+ **/
+
+//constante que estabelece o tamanho da string de entrada
+#define TAMANHO_MAXIMO_DA_ENTRADA 36
+
+//configurando para 9600 o baud rate da Serial do Arduino
 void setup() {
   Serial.begin(9600);
 }
@@ -7,7 +49,7 @@ void setup() {
 // vetor que armazena os dados vindos da Serial
 char string_entrada[TAMANHO_MAXIMO_DA_ENTRADA];
 
-//vetor temporário que armazena o deslocamentos
+// vetor temporário que armazena o deslocamentos
 char tmp[TAMANHO_MAXIMO_DA_ENTRADA] = "";
 
 // string de copia para não afetar a original
@@ -20,8 +62,8 @@ struct string_com_posicao {
 };
 
 // Vetor que armazenará cada linha rotacionada da string e suas posições originais
-// Espaço alocado de 1296 chars, pois é um vetor de string, ou seja,
-// Cada string contem 36 chars, e o vetor possui 36 strings.
+// Espaço alocado de 1369 chars, pois é um vetor de string, ou seja,
+// Cada string contem 37 chars, e o vetor possui 37 strings.
 struct string_com_posicao lista_de_strings[TAMANHO_MAXIMO_DA_ENTRADA];
 
 // Salva as posições ordenadas da lista rotacionada
@@ -31,6 +73,7 @@ int len, new_idx;
 int i = 0, j = 0, k = 0;
 int qtd_posicoes = 0;
 
+// Função que desloca a string em x posições
 void desloca_posicao()
 {
     strcpy(copia, string_entrada);
@@ -43,7 +86,6 @@ void desloca_posicao()
         tmp[new_idx] = copia[i];
     }
     memcpy(copia, tmp, len);
-    //return tmp;
 }
 
 // Função de comparação para ordenar as string em ordem alfabética
@@ -78,9 +120,6 @@ void burrows_wheeler() {
     }
 }
 
-unsigned long tempoAntesDaTransformada, tempoDepoisDaTransformada;
-
-
 void loop() {
   if(Serial.available() > 0){
     memset(string_entrada, 0, sizeof(string_entrada));
@@ -95,11 +134,8 @@ void loop() {
       Serial.print("Texto a ser tranformado: ");
       Serial.println(string_entrada);
       Serial.print("Método de Burrows-Wheeler: ");
-      tempoAntesDaTransformada = micros();
       burrows_wheeler();
-      tempoDepoisDaTransformada = micros();
       Serial.println(string_rotacionada);
-      Serial.println(tempoDepoisDaTransformada - tempoAntesDaTransformada);
     }
   }
 }
